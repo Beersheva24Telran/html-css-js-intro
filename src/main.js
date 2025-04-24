786579d05d7d5df3cd0451ca2bf780f8d1a5fedd
Yuragranovsky <yuragranovsky@gmail.com>
2025-04-24 11:36:56 +0300

    form processing

--- src/main.js
+++ src/main.js
@@ -1,33 +1,6 @@
-//References to DOM
-const tBodyElem = document.getElementById("tbody_id");
-{
-  /* <tr>
-  <th>id</th>
-  <th>Name</th>
-  <th>Department</th>
-  <th>Salary</th>
-</tr> */
-}
-//Variable & constants
-const employees = [
-  { id: 123, name: "Vasya", department: "QA", salary: 10000 },
-  { id: 124, name: "Sara", department: "Audit", salary: 15000 },
-  { id: 125, name: "David", department: "Development", salary: 12000 },
-];
-//functions
-function getTableData(employees) {
-  const res = employees.map(getRecord);
-  return res;
-}
-function getRecord(empl) {
-  const res = `<tr>
-                    <td>${empl.id}</td>
-                    <td>${empl.name}</td>
-                    <td>${empl.department}</td>
-                    <td>${empl.salary}</td>
-                 <tr>`;
-  return res;
-}
-//actions
-const tableData = getTableData(employees);
-tBodyElem.innerHTML = tableData.join("");
+const formElement = document.getElementById("form_id");
+formElement.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(formElement);
+    console.log(Object.fromEntries(formData))
+})
