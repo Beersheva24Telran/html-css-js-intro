80841976516c6e74fbb077f2223522b820641faa
Yuragranovsky <yuragranovsky@gmail.com>
2025-04-28 10:50:59 +0300

    pseudo classes elements

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const formElement = document.getElementById("form_id");
-formElement.addEventListener('submit', event => {
-    event.preventDefault();
-    const formData = new FormData(formElement);
-    console.log(Object.fromEntries(formData))
-})
