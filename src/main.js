5e1f85633cdcb84d886dd0f69e80917dec32ec60
Yuragranovsky <yuragranovsky@gmail.com>
2025-05-05 20:53:34 +0300

    showing detailed info by thumbnail's click

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
+//refernces to the HTML elements
 const galleryItems = document.querySelectorAll(".gallery_item");
-galleryItems.forEach((item,index) => item.addEventListener("click", () => console.log("clicked ", index)))
+const detailedContainerImage = document.querySelector(".detailedContainer_image");
+const detailedContainerTitle = document.querySelector(".detailedContainer_title");
+//functions
+function setDetails(itemElement) {
+    detailedContainerImage.src = itemElement.getAttribute("data-detailed-image");
+    detailedContainerTitle.innerHTML = itemElement.getAttribute("data-detailed-title");
+}
+galleryItems.forEach((item,index) => item.addEventListener("click", () => setDetails(galleryItems[index])));
